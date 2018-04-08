7e379adb7c127e4806463b61d96255fef7a7f0e5
Isaac Duarte <iduarte@cs.stonybrook.edu>
2018-04-08 09:00:40 -0700

    Added support for flags in part 1

--- part1.c
+++ part1.c
@@ -2,8 +2,7 @@
 #include "includes.h"
 
 int main(int argc, char** argv){
-	char* args[2];
-	args[1] = NULL;
+	char* args[6];
 	char buf[256];
 	pid_t pid;
 
@@ -12,13 +11,23 @@ int main(int argc, char** argv){
 		printf("prompt> ");
 		fgets(buf, 256, stdin);
 		buf[strlen(buf) - 1] = '\0';
+		int len = strlen(buf);
 
 		if (strcmp(buf, "exit") == 0){
 			return 0;
 		} else if (strcmp(buf, "help") == 0){
 			printf("exit - quits application\nhelp - prints this message\nrun X - execute application X\n");
 		} else if (strncmp(buf, "run", 3) == 0){
-			args[0] = &buf[4];
+			int i = 3;
+		   	int j = 0;
+			for (i; i < len; i++){
+				if (buf[i] == ' '){
+					buf[i] = '\0';
+					args[j] = &buf[i+1];
+					j++;
+				}
+			}
+			args[j] = NULL;
 			if ((pid = fork()) == 0){
 				execvp(args[0], args);
 				exit(0);
